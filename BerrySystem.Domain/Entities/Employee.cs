392f92d550bb8f2b53088170f5edbaaca5133638
jonvabalis <tmotiejus@gmail.com>
2024-12-27 00:50:59 +0200

    feat: added Employee class

---
+++ BerrySystem.Domain/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace BerrySystem.Domain.Entities;
+
+public class Employee : Entity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime Birthday { get; set; }
+}
