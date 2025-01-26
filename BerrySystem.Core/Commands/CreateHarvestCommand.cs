0a10e562a629846dfa8fe740449067785ce74269
jonvabalis <tmotiejus@gmail.com>
2025-01-26 16:35:21 +0200

    feat: updated create harvest with berryType and berryKind

--- BerrySystem.Core/Commands/CreateHarvestCommand.cs
+++ BerrySystem.Core/Commands/CreateHarvestCommand.cs
@@ -6,4 +6,6 @@ public class CreateHarvestCommand : IRequest<Guid>
 {
     public double Kilograms { get; set; }
     public Guid EmployeeId { get; set; }
+    public Guid BerryTypeId { get; set; }
+    public Guid? BerryKindId { get; set; }
 }
--- BerrySystem.Core/Handlers/Harvest/CreateHarvestCommandHandler.cs
+++ BerrySystem.Core/Handlers/Harvest/CreateHarvestCommandHandler.cs
@@ -10,11 +10,35 @@ public class CreateHarvestCommandHandler(BerrySystemDbContext berrySystemDbConte
 {
     public async Task<Guid> Handle(CreateHarvestCommand request, CancellationToken cancellationToken)
     {
+        var berryType = await berrySystemDbContext.BerryTypes
+            .FindAsync([request.BerryTypeId], cancellationToken);
+
+        if (berryType == null)
+            throw new Exception($"BerryType with ID {request.BerryTypeId} not found.");
+
+        BerryKind? berryKind = null;
+        if (request.BerryKindId.HasValue)
+        {
+            berryKind = await berrySystemDbContext.BerryKinds
+                .FindAsync([request.BerryKindId.Value], cancellationToken);
+
+            if (berryKind == null)
+                throw new Exception($"BerryKind with ID {request.BerryKindId} was not found.");
+        }
+        
+        //TODO: Validation
+        // if (berryKind != null && berryKind.BerryType.Id != berryType.Id)
+        // {
+        //     throw new Exception("The specified BerryKind does not belong to the given BerryType.");
+        // }
+        
         var harvest = new Domain.Entities.Harvest
         {
             Kilograms = request.Kilograms,
             EmployeeId = request.EmployeeId,
             EventTime = DateTime.UtcNow,
+            BerryType = berryType,
+            BerryKind = berryKind,
         };
         
         await berrySystemDbContext.Harvests.AddAsync(harvest, cancellationToken);
