80259cbf1e47969ed56329ae6253af4f759fdc38
jonvabalis <tmotiejus@gmail.com>
2025-06-13 20:49:28 +0300

    fix: consistent error structure

--- BerrySystem.Core/Validation/ExceptionMappingMiddleware.cs
+++ BerrySystem.Core/Validation/ExceptionMappingMiddleware.cs
@@ -13,27 +13,23 @@ public class ExceptionMappingMiddleware(RequestDelegate next, ILogger<ExceptionM
         catch (ValidationException exception)
         {
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            var validationFailureResponse = new ValidationFailureResponse
-            {
-                Errors = exception.Errors.Select(x => new ValidationResponse
-                {
-                    PropertyName = x.PropertyName,
-                    Message = x.ErrorMessage
-                })
-            };
-            await context.Response.WriteAsJsonAsync(validationFailureResponse);
+            var errors = exception.Errors
+                .Select(e => new { message = e.ErrorMessage })
+                .ToList();
+            await context.Response.WriteAsJsonAsync(new { errors });
         }
         catch (NotFoundException ex)
         {
             logger.LogError(ex, "Not found exception");
             context.Response.StatusCode = StatusCodes.Status404NotFound;
-            await context.Response.WriteAsJsonAsync(new { ex.Message });
+            await context.Response.WriteAsJsonAsync(new { errors = new[] { new { message = ex.Message } } });
         }
         catch (Exception ex)
         {
             logger.LogError(ex, "Unhandled exception");
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            await context.Response.WriteAsJsonAsync(new { Message = "Internal Server Error" });
+            await context.Response.WriteAsJsonAsync(
+                new { errors = new[] { new { message = "Internal Server Error" } } });
         }
     }
 }
